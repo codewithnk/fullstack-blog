7f9f7ccad820e179e14caa3b0804ce0d1210ae8a
nileshottoedge <158583219+nileshottoedge@users.noreply.github.com>
2026-01-20 15:10:48 +0530

    port remove

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,13 @@ import app from "../src/app.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen( () => console.log(`Server running `));
   })
   .catch((err) => {
     console.error("DB connection failed", err);
