0ca45f75e45c6dcaf3b3171eaf656f6014edfc73
nileshottoedge <158583219+nileshottoedge@users.noreply.github.com>
2026-01-20 12:31:48 +0530

    url change

--- frontend/src/app/apiSlice.js
+++ frontend/src/app/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: "https://fullstack-blog-i2dk.onrender.com/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.accessToken;
