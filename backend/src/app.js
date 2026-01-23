7209dada2d583c13cb0990b019c27b7e1d63d65a
nileshottoedge <158583219+nileshottoedge@users.noreply.github.com>
2026-01-23 11:26:51 +0530

    not found issue solved

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,7 +34,7 @@ app.get("/api/testing", (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(
     path.join(__dirname, "../../frontend/build", "index.html")
   );
