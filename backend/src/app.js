1e5f2af93fadd76b8309c7f9343ff8fc8ccd4b34
nileshottoedge <158583219+nileshottoedge@users.noreply.github.com>
2026-01-23 14:23:38 +0530

    remove * from app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,28 +2,26 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 import { errorHandler } from "./middlewares/errormiddleware.js";
 import authRoutes from "./routes/authroutes.js";
 import postRoutes from "./routes/postroutes.js";
 import commentRoutes from "./routes/commentroutes.js";
 import adminRoutes from "./routes/adminroutes.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
-
-const allowedOrigins = [
-  "https://fullstack-blog-1-n5qh.onrender.com",
-];
-
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(
+  cors({
+    origin: ["https://fullstack-blog-1-n5qh.onrender.com"],
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
@@ -33,8 +31,9 @@ app.get("/api/testing", (req, res) => {
   res.json({ status: "OK", message: "Server is running" });
 });
 const clientPath = path.join(__dirname, "../../frontend/dist");
+
 app.use(express.static(clientPath));
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(clientPath, "index.html"));
 });
 app.use(errorHandler);
